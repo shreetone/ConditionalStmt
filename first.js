77a9a451d7d26f442f667fb38c048d28905b0bd3
Shree <shriharitone3@gmail.com>
2025-06-05 19:16:34 +0530

    javascript

---
+++ first.js
@@ -0,0 +1,126 @@
+/* var clg="Willingdon";
+console.log(clg);
+
+ age=22;
+console.log(age);
+
+let color="white";
+console.log(color);
+let col="black";
+console.log(col);
+
+
+ if (false) {
+let message = "Hello!";
+console.log(message); 
+}
+else{
+message="Bye";
+console.log(message); 
+}
+
+const fruit=['Banana','mango','apple'];
+fruit.push('pineapple');
+console.log(fruit) ; 
+
+
+ x=5
+if(x==5){
+ var name="wisdosm Sprouts";
+}
+
+ a=Number(prompt("Enter Your First Number"));
+ b=Number(prompt("Enter Your First Number"));  
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b); */
+
+/* console.log("first greater than second",a>b);
+console.log("first less than second",a<b);
+console.log("Are two number are equal?",a===b);
+console.log("first greater than second",a!=b);
+ */
+
+ /*  Math=80;
+  science=90;
+  if(Math>90 && science>=85){
+    console.log("Eligible");
+  }
+  else{
+    console.log("Not Eligible");
+  }
+ */
+ /*  let isAdult=true;
+  let hasLicence=false;
+
+
+  console.log(isAdult && hasLicence);
+  console.log(isAdult || hasLicence);
+  console.log(!isAdult); */
+
+/* x=10;
+5=x;
+console.log("Initial Value Of X:",x);
+console.log("Initial Value Of X:",x+5);
+console.log("Initial Value Of X:",x*3);
+console.log("Initial Value Of X:",x);
+console.log("Initial Value Of X:",x); */
+
+/* isLogin=false
+if(isLogin){
+  console.log("You Already Logged In");
+}else{
+  console.log("Please Login");
+} */
+
+/* age=17;
+if(age>18){
+  console.log("Eligible for Vote");
+}else{
+  console.log("Not Eligible for Vote")
+} */
+
+/* marks=34;
+if(marks <35){
+  console.log("Fail");
+}else if(marks>35 && marks<60){
+  console.log("Pass");
+}else if(marks>60 && marks<75){
+  console.log("Pass with first class");
+
+}else if(marks>75 && marks<100){
+  console.log("Pass with Distinct");
+}
+ */
+
+/* let i= 1;
+for(let i=1;i<=5;i++)
+{
+  console.log(i);
+}
+
+n1=prompt("Enter The Number");
+
+for(let n1;n1>0){
+    console.log();
+} */
+
+
+/* let num =4;
+if(num % 2==0){
+  console.log(" The Number is Even");
+}else{
+  console.log("Number is Odd");
+} */
+
+
+let fact;
+let number;
+
+for(let i=1;i<=number;i++){
+  fact=i*fact;
+}
