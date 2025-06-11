5ae44b33036cb5abd5eea402e675e48b450dba6a
Shree <shriharitone3@gmail.com>
2025-06-11 14:54:33 +0530

    first commit

---
+++ Operators.js
@@ -0,0 +1,100 @@
+
+//Arithmetic Operators
+a=Number(prompt("Enter Your First Number"));
+b=Number(prompt("Enter Your First Number"));  
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+//Comparision Operators
+
+ a=Number(prompt("Enter Your First Number"));
+b=Number(prompt("Enter Your First Number"));
+
+console.log("first greater than second",a>b);
+console.log("first less than second",a<b);
+console.log("Are two number are equal?",a===b);
+console.log("first greater than second",a!=b);
+
+
+// LOgocal Operator
+
+Math=80;
+  science=90;
+  if(Math>90 && science>=85){
+    console.log("Eligible");
+  }
+  else{
+    console.log("Not Eligible");
+  }
+
+//Assignment Operator
+// Declare variable x and assign initial value of 10
+
+let x = 10;
+console.log("Adding :",x += 5); 
+console.log("After Multiplyaing By 3:",x *= 3); 
+console.log(" After Subtracting 10:",x -= 10);  
+console.log("Divide x by 2:",x /= 2); 
+const modulus = x % 3; 
+console.log("Modulus of x with 3:", modulus); 
+
+
+// Operator Precedence
+console.log("10 + 3 * 5 =", 10 + 3 * 5);        
+console.log("(10 + 3) * 5 =", (10 + 3) * 5);   
+console.log("20 / 2 + 3 =", 20 / 2 + 3);        
+console.log("20 / (2 + 3) =", 20 / (2 + 3));    
+
+
+// Combined Example
+    function calculateRectangle() {
+        const length = parseFloat(prompt("Enter length:"));
+        const width = parseFloat(prompt("Enter width:"));
+        const perimeter = 2 * (length + width);
+        const area = length * width;
+        const PerimeterGreater = perimeter > area;
+        console.log("Perimeter:", perimeter);
+        console.log("Area:",area);
+        console.log("Is perimeter greater than area?",PerimeterGreater);
+    }
+    calculateRectangle();
+
+//  Voting Eligibility Checker
+function checkVotingEligibility() {
+    const age = parseInt(prompt("Enter age:"));
+    const citizenshipInput = prompt("Are you a citizen?");
+    const citizenship = citizenshipInput.toLowerCase() === 'true';
+    const isEligible = age >= 18 && citizenship;
+    const isNotEligible = !isEligible;
+    console.log("Eligible to vote:",isEligible);
+}
+checkVotingEligibility();
+
+// Even or Odd
+function checkEvenOdd() {
+    const number = parseInt(prompt("Enter a number:"));
+    if (number % 2 === 0) {
+        console.log(`The number is even.`);
+    } else {
+        console.log(`The number is odd.`);
+    }
+}
+checkEvenOdd();
+
+
+
+// Calculate shopping discount
+function calculateDiscount() {
+    let total = parseFloat(prompt("Enter total amount:"));
+    if (total > 100) {
+        total *= 0.9;
+    }
+    console.log("Discounted total:",total.toFixed(2));
+}
+calculateDiscount();
+
--- first.js
+++ first.js
@@ -37,15 +37,15 @@ console.log(a-b);
 console.log(a*b);
 console.log(a/b);
 console.log(a%b);
-console.log(a**b); */
+console.log(a**b);
 
-/* console.log("first greater than second",a>b);
+console.log("first greater than second",a>b);
 console.log("first less than second",a<b);
 console.log("Are two number are equal?",a===b);
 console.log("first greater than second",a!=b);
- */
 
- /*  Math=80;
+
+  Math=80;
   science=90;
   if(Math>90 && science>=85){
     console.log("Eligible");
@@ -53,38 +53,38 @@ console.log("first greater than second",a!=b);
   else{
     console.log("Not Eligible");
   }
- */
- /*  let isAdult=true;
+
+  let isAdult=true;
   let hasLicence=false;
 
 
   console.log(isAdult && hasLicence);
   console.log(isAdult || hasLicence);
-  console.log(!isAdult); */
+  console.log(!isAdult);
 
-/* x=10;
+x=10;
 5=x;
 console.log("Initial Value Of X:",x);
 console.log("Initial Value Of X:",x+5);
 console.log("Initial Value Of X:",x*3);
 console.log("Initial Value Of X:",x);
-console.log("Initial Value Of X:",x); */
+console.log("Initial Value Of X:",x);
 
-/* isLogin=false
+isLogin=false
 if(isLogin){
   console.log("You Already Logged In");
 }else{
   console.log("Please Login");
-} */
+}
 
-/* age=17;
+age=17;
 if(age>18){
   console.log("Eligible for Vote");
 }else{
   console.log("Not Eligible for Vote")
-} */
+}
 
-/* marks=34;
+marks=34;
 if(marks <35){
   console.log("Fail");
 }else if(marks>35 && marks<60){
@@ -95,9 +95,9 @@ if(marks <35){
 }else if(marks>75 && marks<100){
   console.log("Pass with Distinct");
 }
- */
 
-/* let i= 1;
+
+let i= 1;
 for(let i=1;i<=5;i++)
 {
   console.log(i);
@@ -107,15 +107,15 @@ n1=prompt("Enter The Number");
 
 for(let n1;n1>0){
     console.log();
-} */
+}
 
 
-/* let num =4;
+let num =4;
 if(num % 2==0){
   console.log(" The Number is Even");
 }else{
   console.log("Number is Odd");
-} */
+}
 
 
 let fact;
@@ -123,4 +123,111 @@ let number;
 
 for(let i=1;i<=number;i++){
   fact=i*fact;
+}
+ */
+
+
+/* let color="red";
+if(color=="red"){
+  console.log("Stop");
+}
+if(color=="yellow"){
+  console.log("Go Slow");
+}
+if(color=="green"){
+  console.log("Go")
+} */
+
+/* let size="L";
+if(size=="XL"){
+  console.log("250");
+}else if(size=="L"){
+  console.log("200");
+}else if(size=="M"){
+  console.log("100");
+}else if(size=="M"){
+  console.log("50");
+}else{
+  console.log("Not Available");
+} */
+
+/*   let str="apple";
+  if(str[0]==="a"&& str.length>3){
+    console.log("Good String")
+  }else{
+    console.log("Not a Good String");
+  } */
+
+/* let day = 1;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wedensday");
+    break;
+  case 4:
+    console.log("Thursdat");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+
+    default:
+      console.log("");
+
+
+
+}
+
+
+ */
+
+/* n=5;
+
+for(i=1;i<=n;i++){
+  line='';
+  for(j=1;j<=5;j++){
+    line+="*";
+  }
+  console.log(line);
+} */
+
+/* n=5;
+
+for(i=1;i<=n;i++){
+  line='';
+  for(j=1;j<=i;j++){
+    line+="*";
+  }
+  console.log(line);
+} */
+
+/* for(i=1;i>=1;i--){
+  line='';
+  for(j=1;j<=i;j++){
+    line-=" *";
+  }
+  console.log(line);
+} */
+
+n=5;
+for(i=1;i<=n;i++){
+  line=' ';
+  for(j=1;j<=n-i;j++){
+    line+=' ';
+  }
+  for(j=1;j<=i;j++){
+    line+=' *'
+  }
+  console.log(line);
 }
